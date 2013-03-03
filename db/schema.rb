a45fee59d4a0b2cee2c45ea739dfcd224c2b8d07
mikael theimer <mikaeltheim@gmail.com>
2013-03-03 15:47:17 -0500

    fixed pins with images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130228005114) do
+ActiveRecord::Schema.define(:version => 20130303203736) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
